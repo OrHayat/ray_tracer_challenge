90ae28bcc4207db1434a1c227df5ab85211a7c80
OrHayat <you@example.com>
2020-09-17 18:19:09 +0300

    model

--- src/rtc/core/shape.hpp
+++ src/rtc/core/shape.hpp
@@ -6,7 +6,9 @@
 #define RAY_TRACER_SHAPE_HPP
 #include <rtc/core/collision_data.hpp>
 #include <rtc/core/ray.hpp>
+#include <glm/glm.hpp>
 struct shape{
+    glm::mat4 model;
     virtual collision_data collide(ray r)=0;
 };
 #endif //RAY_TRACER_SHAPE_HPP
