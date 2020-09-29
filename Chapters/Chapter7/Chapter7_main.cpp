6b770215825ac8728f14d28ba37c9e8e2568cbc7
OrHayat <hayatb@post.bgu.ac.il>
2020-09-29 23:12:57 +0300

    plane and disk work

--- Chapters/Chapter7/Chapter7_main.cpp
+++ Chapters/Chapter7/Chapter7_main.cpp
@@ -21,7 +21,8 @@ int main(int argc, char** argv)
 {
     Scene s;
     Camera cam(650,400,glm::pi<float>()/2);
-    cam.set_transform(glm::scale(glm::translate(glm::mat4(),glm::vec3(0,0,-4)),glm::vec3(-1,-1,1)));
+    cam.set_transform(glm::lookAt(glm::vec3(1,4,2),glm::vec3(0),glm::vec3(0,1,0)));
+    //cam.set_transform(glm::scale(glm::translate(glm::mat4(),glm::vec3(0,0,-4)),glm::vec3(-1,-1,1)));
     s.I_ambient=glm::vec4(0.6f,0.4f,0.5f,1.0f);
     s.selected_camera=0;
     s.cameras.push_back(cam);
@@ -29,7 +30,7 @@ int main(int argc, char** argv)
     sphere* sphere_2= new sphere(2);
 
 //    disk* disk_=new plane(glm::vec3(0),glm::vec3(0,1,0));
-    plane* disk_=new plane(glm::vec3(0),glm::vec3(0,1,0));
+    disk* disk_=new disk(glm::vec3(0,-0.52,0),glm::vec3(0,1,0),2);
 
     material mat_;
     mat_.ka=glm::vec4(0.85f,0.35f,0.2f,1.0f);
@@ -38,18 +39,21 @@ int main(int argc, char** argv)
     mat_.shininess=250.0f;
     sphere_->mat=mat_;
     disk_->mat=mat_;
-    disk_->mat.ka=glm::vec4(0.5f,0,1.0f,1.0f);
+    disk_->mat.ka=glm::vec4(0.15f,0,1.0f,1.0f);
     disk_->mat.kd=glm::vec4(1.0f);
-    disk_->mat.ks=glm::vec4(1,0.5,0.2,1);
+    disk_->mat.ks=glm::vec4(1,0.5,0.72,1);
     disk_->set_model(glm::mat4(1.0f));
     sphere_->set_model(glm::mat4(1.0f));
-    sphere_2->set_model(glm::translate(glm::mat4(),glm::vec3(1,1,3)));
+    sphere_2->set_model(glm::translate(glm::mat4(),glm::vec3(1,1,-4)));
+    sphere_2->mat.ka=glm::vec4(1.0f,0,0,1.0f);
+    sphere_2->mat.kd=glm::vec4(1.0f);
+
 //    sphere_->set_model((glm::translate(glm::mat4(),glm::vec3(0,0,0))*glm::scale(glm::mat4(),glm::vec3(1,1,1))));
 //    sphere_->model=(glm::translate(glm::mat4(),glm::vec3(2,0,0))*glm::scale(glm::mat4(),glm::vec3(3,3,3)));
 //    sphere_->model_inv=glm::inverse(sphere_->model);
-//    s.objects.push_back(sphere_);
-//    s.objects.push_back(sphere_2);
+    s.objects.push_back(sphere_);
     s.objects.push_back(disk_);
+    s.objects.push_back(sphere_2);
     glm::vec3 light1_pos=glm::vec3(0,5.0f,4.0f);;
     light* point_light1=light::make_point_light_ptr(light1_pos);
     point_light1->kc=1.2f;
